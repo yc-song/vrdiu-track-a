c51d3dfe73ada12f83feccbbd12a9e1621926315
Jonghyun (Jong) Song <57248081+yc-song@users.noreply.github.com>
2024-07-30 14:35:13 +0900

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+pip install gdown
+pip install -U datasets
+pip install -U transformers
+pip install seqeval==1.2.2
+pip install sentencepiece
+pip install timm==0.4.12
+pip install Pillow
+pip install einops
+pip install textdistance
+pip install shapely
+pip install numpy
+pip install accelerate -U
+python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
+git clone https://github.com/microsoft/unilm.git
+cd unilm/layoutlmv3
+pip install -e unilm/layoutlmv3
